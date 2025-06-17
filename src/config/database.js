5d5282b7276cdf53d726bbb023a27a69804100ad
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2025-06-17 20:36:05 -0300

    Initial commit - proyecto limpio y seguro

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB conectado exitosamente');
+  } catch (error) {
+    console.error('Error de conexión a MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
